cb7d7a379a8029ead67ce07b17f60e8c829f39a1
dabik9 <93473860+dabik9@users.noreply.github.com>
2025-10-02 20:57:41 +0300

    Добавлено: Класс и меню 1.0

---
+++ Java-learning/src/architecture/main/Main.java
@@ -0,0 +1,134 @@
+package architecture.main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start!");
+        Scanner CONSOLE = new Scanner(System.in);
+        boolean flag = true;
+        boolean login = false;
+
+        int id = 0;
+        String username = null;
+        String email = null;
+        String password = null;
+        String role = "user";
+
+
+        while (flag) {
+            System.out.println("Меню: ");
+            System.out.println();
+            System.out.println("""
+                    1. Регистрация \
+                    
+                    2. Вход \
+                    
+                    3. Информация об аккаунте \
+                    
+                    4. Выход \
+                    
+                    5. Убить систему
+                    """);
+
+            System.out.print("Выберите команду: ");
+            int choice = CONSOLE.nextInt();
+            CONSOLE.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println();
+                    if (login) {
+                        System.out.println("Вы уже вошли в аккаунт!\n");
+                        break;
+                    }
+
+                    System.out.println("Регистрация!\n");
+                    System.out.print("Введите никнейм: ");
+                    String usernameRegister = CONSOLE.nextLine();
+                    System.out.print("Введите почту: ");
+                    String emailRegister = CONSOLE.nextLine();
+                    System.out.print("Введите пароль: ");
+                    String passwordRegister = CONSOLE.nextLine();
+
+                    id++;
+                    username = usernameRegister;
+                    email = emailRegister;
+                    password = passwordRegister;
+
+                    System.out.println("Регистрация прошла успешно!\n");
+                    break;
+
+                case 2:
+                    System.out.println();
+                    if (login) {
+                        System.out.println("Вы уже вошли в аккаунт!\n");
+                        break;
+                    }
+
+                    if (username == null || password == null || email == null) {
+                        System.out.println("Аккаунт не найден! Зарегистрируйтесь!\n");
+                        break;
+                    }
+
+                    System.out.println("Вход!\n");
+                    System.out.print("Введите никнейм: ");
+                    String usernameLogin = CONSOLE.nextLine();
+                    System.out.print("Введите пароль: ");
+                    String passwordLogin = CONSOLE.nextLine();
+
+                    if (!usernameLogin.equals(username) || !passwordLogin.equals(password)) {
+                        System.out.println("Неверный никнейм или пароль!\n");
+                        break;
+                    }
+
+                    System.out.print("Вход выполнен успешно!\n");
+                    login = true;
+                    break;
+
+                case 3:
+                    System.out.println();
+                    if (!login) {
+                        System.out.println("Вы не вошли в аккаунт!\n");
+                        break;
+                    }
+
+                    System.out.println("Информация об аккаунте: " + username + "!\n");
+                    System.out.println(
+                            "ID: " + id + "\n" +
+                            "Никнейм: " + username + "\n" +
+                            "Почта: " + email + "\n" +
+                            "Пароль: " + password + "\n" +
+                            "Роль: " + role + "\n"
+                    );
+
+                case 4:
+                    System.out.println();
+                    if (!login) {
+                        System.out.println("Вы не вошли в аккаунт!\n");
+                        break;
+                    }
+
+                    System.out.println("Выход!\n");
+                    login = false;
+                    break;
+
+                case 5:
+                    System.out.println();
+                    if (role.equals("admin")) {
+                        System.out.println("Система убита!");
+                        flag = false;
+                    }
+                    else {
+                        System.out.println("У Вас недостаточно прав для выполнения этой команды!\n");
+                    }
+                    break;
+
+                default:
+                    System.out.println();
+                    System.out.println("Команда не распознана!\n");
+                    break;
+            }
+        }
+    }
+}
